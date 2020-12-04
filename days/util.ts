bc0b3d0f270ed9d89dfcd88e02eb44cdb5527121
Mike Kitzman <mdkitzman@gmail.com>
2020-12-04 08:47:58 -0700

    Day 4

--- days/util.ts
+++ days/util.ts
@@ -4,6 +4,7 @@ import {bfsFromNode} from 'graphology-traversal'
 
 export const sum = (prev: number, cur: number) => prev + cur;
 export const multiply = (prev: number, cur: number) => prev * cur;
+export const allTrue = (prev: boolean, cur:boolean) => prev && cur;
 
 export const max = (prev: number, cur: number) => Math.max(prev, cur)
 export const min = (prev: number, cur: number) => Math.min(prev, cur)
