c186b593ee6635031ae00dcc3336b874cf8a8961
Mike Kitzman <mdkitzman@gmail.com>
2020-12-16 18:40:36 -0700

    Renaming variables

--- days/16/index.ts
+++ days/16/index.ts
@@ -3,36 +3,26 @@ import { isEmpty } from 'lodash';
 import { allTrue, anyTrue, multiply, sum } from '../util/arrayUtils';
 import { inRange } from './../util/numberUtils';
 
-type Range = [number, number];
-type Column = {
+type Field = {
   name:string;
-  ranges:[Range, Range];
   valueValid:(val:number)=>boolean;
 };
 
-
-const parseInput = (input:string):{columns:Column[], tickets:number[][]} => {
+const parseInput = (input:string):{fields:Field[], tickets:number[][]} => {
   const lines = input.split('\n\n');
-  const columns:Column[] = lines[0].split('\n')
+  const fields:Field[] = lines[0].split('\n')
     .map(line => /([a-zA-Z ]+): (\d+)-(\d+) or (\d+)-(\d+)/.exec(line) || [])
     .map(matches => {
-      const ranges:[Range, Range] = [
-        [parseInt(matches[2], 10), parseInt(matches[3], 10)],
-        [parseInt(matches[4], 10), parseInt(matches[5], 10)],
+      const rangesValidators:((val:number)=>boolean)[] = [
+        inRange(parseInt(matches[2], 10), parseInt(matches[3], 10)),
+        inRange(parseInt(matches[4], 10), parseInt(matches[5], 10)),
       ];
       return {
         name:matches[1],
-        valueValid:(value:number):boolean => {
-          return ranges
-            .map(range => inRange(range[0], range[1])(value)
-          ).reduce(anyTrue);
-        },
-        ranges,
+        valueValid:(value:number):boolean => rangesValidators.some(validator => validator(value)),
       };
     });
   
-  
-
   const myTicket = lines[1].split('\n')[1].split(',').map(val=>parseInt(val,10));
   const nearbyTickets = lines[2].split('\n').filter((_, index) => index !== 0)
                           .map(line => line.split(',').map(val => parseInt(val, 10)));
@@ -41,20 +31,20 @@ const parseInput = (input:string):{columns:Column[], tickets:number[][]} => {
     ...nearbyTickets
   ];
   return {
-    columns,
+    fields,
     tickets
   }
 }
 
-const invalidValues = (columns:Column[], ticket:number[]):number[] => ticket.filter(ticketValue => !columns.map(c => c.valueValid(ticketValue)).reduce(anyTrue));
-const ticketInvalid = (columns:Column[], ticket:number[]):boolean => invalidValues(columns, ticket).length > 0
+const invalidValues = (fields:Field[], ticket:number[]):number[] => ticket.filter(ticketValue => !fields.some(c => c.valueValid(ticketValue)));
+const ticketInvalid = (fields:Field[], ticket:number[]):boolean => invalidValues(fields, ticket).length > 0
 
 const part1 = (input:string) => {
-  const { columns, tickets } = parseInput(input);
+  const { fields, tickets } = parseInput(input);
   
   const invalidvalues = tickets.filter((_, index) => index > 0)
-    .filter(ticket => ticketInvalid(columns, ticket))
-    .map(ticket => invalidValues(columns, ticket))
+    .filter(ticket => ticketInvalid(fields, ticket))
+    .map(ticket => invalidValues(fields, ticket))
     .flat();
 
   console.log(`Part 1 : Error rate is ${invalidvalues.reduce(sum, 0)}`);
@@ -62,40 +52,40 @@ const part1 = (input:string) => {
 
 const part2 = (input:string) => {
 
-  const { columns, tickets } = parseInput(input);
-  const validTickets = tickets.filter(ticket => !ticketInvalid(columns, ticket));
+  const { fields, tickets } = parseInput(input);
+  const validTickets = tickets.filter(ticket => !ticketInvalid(fields, ticket));
 
-  // Find all of the columns that that satisfy the rule for all tickets
-  const columnMap = columns
-    .map(column => {
+  // Find all of the fields that that satisfy the rule for all tickets
+  const fieldmap = fields
+    .map(field => {
       const possibleCols:number[] = [];      
-      for(let iCol = 0; iCol < columns.length; iCol++) {
+      for(let iCol = 0; iCol < fields.length; iCol++) {
         const found = validTickets
           .map(ticket => ticket[iCol])
-          .map(value => column.valueValid(value))
+          .map(value => field.valueValid(value))
           .reduce(allTrue);
         if (found){
           possibleCols.push(iCol);
         }
       }
-      return { [column.name]:possibleCols };
+      return { [field.name]:possibleCols };
     })
     .reduce((prev, cur) => ({ ...prev, ...cur }), {});
 
   // There should be 1 column that has 1 entry, and one with 2, etc.
   // Use the process of elimination to get a single column per rule
-  let reducedColumnMap:{[k:string]:number} = {};
-  while(!isEmpty(columnMap)) {
-    const entryKey = Object.keys(columnMap).filter(key => columnMap[key].length === 1)[0];
-    const column = columnMap[entryKey][0];
-    reducedColumnMap[entryKey] = column;
-    Object.values(columnMap).forEach(columns => columns.splice(columns.indexOf(column), 1));
-    delete columnMap[entryKey];   
+  let reducedFieldMap:{[k:string]:number} = {};
+  while(!isEmpty(fieldmap)) {
+    const entryKey = Object.keys(fieldmap).filter(key => fieldmap[key].length === 1)[0];
+    const column = fieldmap[entryKey][0];
+    reducedFieldMap[entryKey] = column;
+    Object.values(fieldmap).forEach(fields => fields.splice(fields.indexOf(column), 1));
+    delete fieldmap[entryKey];   
   }
   
   const myTicket = tickets[0];
-  const departureValues = Object.entries(reducedColumnMap)
-    .filter(([key, _]) => key.startsWith("departure"))
+  const departureValues = Object.entries(reducedFieldMap)
+    .filter(([fieldName, _]) => fieldName.startsWith("departure"))
     .map(([_, column]) => myTicket[column]);
 
   console.log(`Part 2 : The result is ${departureValues.reduce(multiply)}`);
